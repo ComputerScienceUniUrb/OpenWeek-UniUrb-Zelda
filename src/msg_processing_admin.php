3f1e0380cc9f356438619cc4f26a6f99a0b70cb1
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2018-02-07 19:33:54 +0100

    Add award notifications generation

--- src/msg_processing_admin.php
+++ src/msg_processing_admin.php
@@ -36,6 +36,52 @@ function msg_processing_admin($context) {
 
         return true;
     }
+    else if(starts_with($text, '/awards')) {
+        $parm = extract_command_payload($text);
+        if(!in_array($parm, array('all', 'completed'), true)) {
+            $context->reply("Use <code>all</code> or <code>completed</code> as parameters for this command.");
+            return true;
+        }
+
+        Logger::debug("Awarding with command '{$text}'", __FILE__, $context);
+
+        $leaderboard = db_table_query(sprintf(
+            "SELECT *, (SELECT COUNT(*) FROM `reached_locations` AS rinn WHERE rinn.`id` = `agg`.`id` AND rinn.`location` = '%s') AS `completed` FROM (SELECT `identities`.`id`, `identities`.`telegram_id`, `identities`.`full_name`, COUNT(`reached_locations`.`location`) AS `steps`, SUM(`reached_locations`.`correct_answer`) AS `correct`, MAX(`reached_locations`.`timestamp`) AS `latest_step` FROM `identities` LEFT OUTER JOIN `reached_locations` ON `identities`.`id` = `reached_locations`.`id` WHERE `reached_locations`.`location` NOT IN ('%s') GROUP BY `identities`.`id` HAVING DATE(`latest_step`) = DATE(NOW()) ORDER BY `correct` DESC, `steps` DESC, `latest_step` ASC, `identities`.`id` ASC) AS `agg` HAVING `completed` >= %d ORDER BY `completed` DESC LIMIT 40",
+            db_escape(LOCATION_SELFIE), // identifies completion of track
+            implode("','", LOCATION_IGNORE_IN_COUNT), // locations not to be counted
+            ($parm == 'all') ? 0 : 1
+        ));
+
+        $context->reply("Sending out to " . count($leaderboard) . " users…");
+        Logger::info("Sending out award notifications to " . count($leaderboard) . " users", __FILE__, $context);
+
+        $success = 0;
+        for($i = 0; $i < count($leaderboard); $i++) {
+            $result = telegram_send_message(
+                $leaderboard[$i][1],
+                sprintf(
+                    "Complimenti %s, hai concluso la caccia al tesoro di oggi in <b>%d° posizione</b>! 🎉 Grazie per aver partecipato, ricordati di ritirare il gadget alla premiazione.",
+                    title_case($leaderboard[$i][2]),
+                    $i + 1
+                ),
+                array(
+                    'parse_mode' => 'HTML',
+                    'disable_web_page_preview' => true
+                )
+            );
+
+            if(isset($result['message_id'])) {
+                $success++;
+            }
+            else {
+                Logger::error("Failed sending message to user #" . $leaderboard[$i][0] . ": " . json_encode($result), __FILE__, $context);
+            }
+        }
+
+        $context->reply("Sent <b>{$success}/" . count($leaderboard) . "</b> messages successfully.");
+
+        return true;
+    }
     else if(starts_with($text, '/send ')) {
         $payload = extract_command_payload($text);
         if(!$payload) {
